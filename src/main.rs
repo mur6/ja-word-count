bad8054f7c30c445d175adba4500f0a6eb9dfe48
MURAKI Taichi <muraki.taichi@gmail.com>
2020-12-18 14:35:25 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 use sudachiclone::prelude::*;
+use std::env;
+use std::fs;
+
 
 fn main() {
     let dictionary = Dictionary::setup(None, None).unwrap();
     let tokenizer = dictionary.create();
 
-    // Multi-granular tokenization
-    // using `system_core.dic` or `system_full.dic` version 20190781
-    // you may not be able to replicate this particular example due to dictionary you use
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    //println!("My path is {}.", args[0]);
+    println!("In file {}", filename);
+    let contents = fs::read_to_string(filename).unwrap();
 
-    for m in tokenizer.tokenize("国家公務員", &Some(SplitMode::C), None).unwrap() {
-        println!("{}", m.surface());
-    };
-    //# => 国家公務員
-    //println!("Hello, world!");
+    let mut count = 0;
+    for line in contents.lines() {
+        for contents in line.split_whitespace() {
+            for m in tokenizer.tokenize(contents, &Some(SplitMode::C), None).unwrap() {
+                //println!("{}", m.surface());
+                count += 1;
+            };
+        }
+    }
+    println!("{}", count);
 }
