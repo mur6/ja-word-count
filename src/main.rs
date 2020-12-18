8c83b2eabd5ac9821487155bfe1faf63fc7e597a
MURAKI Taichi <muraki.taichi@gmail.com>
2020-12-18 15:05:20 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,53 @@
-use sudachiclone::prelude::*;
 use std::env;
 use std::fs;
 
+use lindera::tokenizer::Tokenizer;
+use lindera_core::core::viterbi::Mode;
 
 fn main() {
-    let dictionary = Dictionary::setup(None, None).unwrap();
-    let tokenizer = dictionary.create();
+    let mut tokenizer = Tokenizer::new(Mode::Normal, "");
 
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
     //println!("My path is {}.", args[0]);
     println!("In file {}", filename);
     let contents = fs::read_to_string(filename).unwrap();
-
+    println!("Contents opened.");
     let mut count = 0;
     for line in contents.lines() {
-        for contents in line.split_whitespace() {
-            for m in tokenizer.tokenize(contents, &Some(SplitMode::C), None).unwrap() {
-                //println!("{}", m.surface());
-                count += 1;
-            };
-        }
+        let tokens = tokenizer.tokenize(line);
+        count += tokens.len();
+        // for token in  {
+        //     let a  = token.text;
+        //     println!("{}", a);
+        // }
     }
     println!("{}", count);
 }
+
+
+
+
+
+
+// fn main() {
+//     let dictionary = Dictionary::setup(None, None).unwrap();
+//     let tokenizer = dictionary.create();
+
+//     let args: Vec<String> = env::args().collect();
+//     let filename = &args[1];
+//     //println!("My path is {}.", args[0]);
+//     println!("In file {}", filename);
+//     let contents = fs::read_to_string(filename).unwrap();
+
+//     let mut count = 0;
+//     for line in contents.lines() {
+//         for contents in line.split_whitespace() {
+//             for m in tokenizer.tokenize(contents, &Some(SplitMode::C), None).unwrap() {
+//                 //println!("{}", m.surface());
+//                 count += 1;
+//             };
+//         }
+//     }
+//     println!("{}", count);
+// }
