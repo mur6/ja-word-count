8326323ad4b2e4c4cd1b60c82ba1258c3c2a41ab
MURAKI Taichi <muraki.taichi@gmail.com>
2020-12-18 15:14:19 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ use std::fs;
 use lindera::tokenizer::Tokenizer;
 use lindera_core::core::viterbi::Mode;
 
+fn count_words(tokenizer: & mut Tokenizer, line: &str) -> usize {
+    let tokens = tokenizer.tokenize(line);
+    tokens.len()
+}
+
 fn main() {
     let mut tokenizer = Tokenizer::new(Mode::Normal, "");
 
@@ -13,15 +18,8 @@ fn main() {
     println!("In file {}", filename);
     let contents = fs::read_to_string(filename).unwrap();
     println!("Contents opened.");
-    let mut count = 0;
-    for line in contents.lines() {
-        let tokens = tokenizer.tokenize(line);
-        count += tokens.len();
-        // for token in  {
-        //     let a  = token.text;
-        //     println!("{}", a);
-        // }
-    }
+    //let mut count = 0;
+    let count: usize = contents.lines() .map(|line| count_words(&mut tokenizer, line)).sum();
     println!("{}", count);
 }
 
