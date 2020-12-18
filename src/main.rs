247ad942ebfa2b32483c615970a1b372273c3ef5
MURAKI Taichi <muraki.taichi@gmail.com>
2020-12-18 12:55:15 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use sudachiclone::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let dictionary = Dictionary::setup(None, None).unwrap();
+    let tokenizer = dictionary.create();
+
+    // Multi-granular tokenization
+    // using `system_core.dic` or `system_full.dic` version 20190781
+    // you may not be able to replicate this particular example due to dictionary you use
+
+    for m in tokenizer.tokenize("国家公務員", &Some(SplitMode::C), None).unwrap() {
+        println!("{}", m.surface());
+    };
+    //# => 国家公務員
+    //println!("Hello, world!");
 }
